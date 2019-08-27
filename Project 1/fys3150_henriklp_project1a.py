20c3670e812ac04c6a3e48c77c7638e43c20992a
henrikx2 <hlpetlund@gmail.com>
2019-08-27 11:11:07 +0200

    Started coding 26.07.19

--- Project 1/fys3150_henriklp_project1a.py
+++ Project 1/fys3150_henriklp_project1a.py
@@ -1,4 +1,29 @@
 from numpy import *
+from matplotlib.pyplot import *
 
-for i in g:
-    
+def f(x):
+    return 100*exp(-10*x)
+
+def analyticSolution(x):
+    return 1-(1-exp(-10))*x-exp(-10x)
+
+def generalSolution(a,b,c,d):
+    for i in range(2,n):
+        b[i] = b[i] - c[i-1]*(a[i]/b[i-1])
+        d[i] = d[i] - d[i-1]*(a[i]/b[i-1])
+        a[i] = a[i] - b[i-1]*(a[i]/b[i-1])
+
+    v[n] = s[n]/b[n]
+    for i in range(n-1, 1):
+        
+
+
+def specalSolution():
+
+def LUdecomp():
+
+n = [10,100,1000]
+
+for i in n:
+    h = 1/(n+1)
+    x = linspace(0,1,n)

00d51a710d12b37c889529c52de97a96cff29021
henrikx2 <hlpetlund@gmail.com>
2019-09-03 11:12:07 +0200

    Began working LU and Special

--- Project 1/fys3150_henriklp_project1a.py
+++ Project 1/fys3150_henriklp_project1a.py
@@ -2,6 +2,10 @@ from numpy import *
 from matplotlib.pyplot import *
 import os
 import sys
+import time
+from scipy.linalg import lu_solve, lu_factor
+print("This program solves a linear second order-differential equation by using a tridiagonal matrix. The calculation speeds and relative errors will be determined in the case of a general algorithm, a special algorithm and by the use of LU-decomposition.")
+n = [pow(10,i) for i in range(1,int(input("Choose exponent of 10 (mesh grid): "))+1)] # Choose the number of iterations from power of 10
 
 def f(x):
     return 100*exp(-10*x)
@@ -9,25 +13,62 @@ def f(x):
 def analyticSolution(x):
     return 1-(1-exp(-10))*x-exp(-10*x)
 
-def generalSolution(a,d,c,b,n):
-    for i in range(1,n): #Forward substitution on the interval [1,ng-1]
+def TDMAGeneralSolution(a,d,c,b,n):
+    start_time = time.time()
+    for i in range(1,n): #Forward substitution on the interval [1,n]
         ad = a[i-1]/d[i-1]
         d[i] = d[i] - c[i-1]*(ad)
         b[i+1] = b[i+1] - b[i]*(ad)
 
     u = zeros(len(b))
     u[n] = b[n]/d[n-1]
-    for i in range(n-2,-1,-1): #Backwards substitution on the interval [n-1,0]
+    for i in range(n-2,-1,-1): #Backwards substitution on the interval [n-1,1]
         u[i+1] = (b[i+1] - c[i]*u[i+2])/d[i]
+    elapsed_time = time.time()-start_time
+    print("[TDMA General (n = "+str(n)+")], calculation time: "+str(elapsed_time)+" s")
     return u
 
-def specalSolution():
-    return 0
-def LUdecomp():
-    return 0
+def TDMASpecalSolution(n):
+    start_time = time.time()
+    for i in range(1,n): #Forward substitution on the interval [1,n]
+        ad = -1/d[i-1]
+        d[i] = d[i] + ad
+        b[i+1] = b[i+1] + ad
+
+    u = zeros(len(b))
+    u[n] = b[n]/d[n-1]
+    for i in range(n-2,-1,-1): #Backwards substitution on the interval [n-1,1]
+        u[i+1] = (b[i+1] + u[i+2])/d[i]
+    elapsed_time = time.time()-start_time
+    print("[TDMA Special (n = "+str(n)+")], calculation time: "+str(elapsed_time)+" s")
+    return u
+
+def LUdecomp(n):
+    h = 1/n #Step size
+    n = n-1 #Exclude endpoints
+    A = zeros((n,n))
+    b = zeros(n)
+    x = zeros(n)
+    A[0,0] = 2; A[0,1] = -1; #Set first row of A
+    x = linspace(h,1-h,n); b[0] = h**2*f(x[0])
+    x[n-1] = n*h; b[n-1] = h**2*f(x[n-1])
+    for i in range(1,n-1): #Set middle of A
+        x[i] = x[i] + h
+        b[i] = h**2*f(x[i])
+        A[i,i-1] = -1 #Lower diagona
+        A[i,i] = 2 #Main diagonal
+        A[i,i+1] = -1 #Upper diagonal
+    A[n-1,n-1] = 2; A[n-1,n-2] = -1 #Set last row of A
+    lu, piv = lu_factor(A)
+    start_time = float(time.time())
+    u = lu_solve((lu,piv),b)
+    elapsed_time = time.time() - start_time
+    print("[LU (n = "+str(n+1)+")], calculation time: "+str(elapsed_time)+" s")
+    plot(x,u,label="LU-Decomp, n = "+ str(n+1))
+    return u
 
 #General Solution
-n = [pow(10,i) for i in range(1,int(input("Type power of 10: "))+1)] # Choose the number of iterations from power of 10
+
 for i in n:
     h = 1/(i+2) #Step size
     x = linspace(0,1,i+2) #x-array
@@ -38,15 +79,25 @@ for i in n:
     d_arr += 2 #Numbers on main diagonal
     c_arr = zeros(i)
     c_arr += -1 #Numbers on upper diagonal
-    u_arr = generalSolution(a_arr,d_arr,c_arr,b_arr,i)
+    u_arr = TDMAGeneralSolution(a_arr,d_arr,c_arr,b_arr,i)
     plot(x,u_arr, label = "n = " + str(i))
     if i == n[-1]:
         plot(x,analyticSolution(x), label="Analytical solution")
-legend()
-xlabel("x")
-ylabel("u")
-show()
 
 #Special solution
 
 #LU-decomposition
+
+for i in n:
+    u = LUdecomp(i)
+    if i == n[-1]:
+        x = linspace(0,1,i)
+        plot(x,analyticSolution(x), label="Analytical solution")
+
+#Show plot
+'''
+legend()
+xlabel("x")
+ylabel("u")
+show()
+'''

2d665cf0cffa11ba4c5c04bd92b3a42086276d7f
henrikx2 <hlpetlund@gmail.com>
2019-09-03 13:52:43 +0200

    Starting clean up.

--- Project 1/fys3150_henriklp_project1a.py
+++ Project 1/fys3150_henriklp_project1a.py
@@ -5,7 +5,7 @@ import sys
 import time
 from scipy.linalg import lu_solve, lu_factor
 print("This program solves a linear second order-differential equation by using a tridiagonal matrix. The calculation speeds and relative errors will be determined in the case of a general algorithm, a special algorithm and by the use of LU-decomposition.")
-n = [pow(10,i) for i in range(1,int(input("Choose exponent of 10 (mesh grid): "))+1)] # Choose the number of iterations from power of 10
+n = [pow(10,i) for i in range(1,int(input("Choose exponent of 10 (# of mesh points): "))+1)] # Choose the number of iterations from power of 10
 
 def f(x):
     return 100*exp(-10*x)
@@ -13,8 +13,8 @@ def f(x):
 def analyticSolution(x):
     return 1-(1-exp(-10))*x-exp(-10*x)
 
-def TDMAGeneralSolution(a,d,c,b,n):
-    start_time = time.time()
+def TDMAGeneral(a,d,c,b,n):
+    start_time = time.perf_counter()
     for i in range(1,n): #Forward substitution on the interval [1,n]
         ad = a[i-1]/d[i-1]
         d[i] = d[i] - c[i-1]*(ad)
@@ -24,23 +24,23 @@ def TDMAGeneralSolution(a,d,c,b,n):
     u[n] = b[n]/d[n-1]
     for i in range(n-2,-1,-1): #Backwards substitution on the interval [n-1,1]
         u[i+1] = (b[i+1] - c[i]*u[i+2])/d[i]
-    elapsed_time = time.time()-start_time
-    print("[TDMA General (n = "+str(n)+")], calculation time: "+str(elapsed_time)+" s")
+    elapsed_time = time.perf_counter()-start_time
+    print("[TDMA General (n = "+str(n)+")], CPU Time: "+str(elapsed_time))
     return u
 
-def TDMASpecalSolution(n):
-    start_time = time.time()
-    for i in range(1,n): #Forward substitution on the interval [1,n]
+def TDMASpecial(d,b,n):
+    start_time = time.perf_counter()
+    for i in range(1,n):
         ad = -1/d[i-1]
         d[i] = d[i] + ad
-        b[i+1] = b[i+1] + ad
+        b[i+1] = b[i+1] - b[i]*(ad)
 
     u = zeros(len(b))
     u[n] = b[n]/d[n-1]
-    for i in range(n-2,-1,-1): #Backwards substitution on the interval [n-1,1]
+    for i in range(n-2,-1,-1):
         u[i+1] = (b[i+1] + u[i+2])/d[i]
-    elapsed_time = time.time()-start_time
-    print("[TDMA Special (n = "+str(n)+")], calculation time: "+str(elapsed_time)+" s")
+    elapsed_time = time.perf_counter()-start_time
+    print("[TDMA Special (n = "+str(n)+")], CPU Time: "+str(elapsed_time))
     return u
 
 def LUdecomp(n):
@@ -60,15 +60,15 @@ def LUdecomp(n):
         A[i,i+1] = -1 #Upper diagonal
     A[n-1,n-1] = 2; A[n-1,n-2] = -1 #Set last row of A
     lu, piv = lu_factor(A)
-    start_time = float(time.time())
+    start_time = float(time.perf_counter())
     u = lu_solve((lu,piv),b)
-    elapsed_time = time.time() - start_time
-    print("[LU (n = "+str(n+1)+")], calculation time: "+str(elapsed_time)+" s")
-    plot(x,u,label="LU-Decomp, n = "+ str(n+1))
+    elapsed_time = time.perf_counter() - start_time
+    print("[LU (n = "+str(n+1)+")], CPU Time: "+str(elapsed_time))
     return u
 
+#Analytical solution
+exact
 #General Solution
-
 for i in n:
     h = 1/(i+2) #Step size
     x = linspace(0,1,i+2) #x-array
@@ -79,20 +79,33 @@ for i in n:
     d_arr += 2 #Numbers on main diagonal
     c_arr = zeros(i)
     c_arr += -1 #Numbers on upper diagonal
-    u_arr = TDMAGeneralSolution(a_arr,d_arr,c_arr,b_arr,i)
+    u_arr = TDMAGeneral(a_arr,d_arr,c_arr,b_arr,i)
+    exact_arr = analyticSolution(x)
+    #Plot
     plot(x,u_arr, label = "n = " + str(i))
     if i == n[-1]:
-        plot(x,analyticSolution(x), label="Analytical solution")
+        plot(x,exact_arr, label="Analytical solution")
 
 #Special solution
+for i in n:
+    h = 1/(i+2) #Step size
+    x = linspace(0,1,i+2) #x-array
+    b_arr = h**2*f(x) #Source term
+    d_arr = zeros(i)
+    d_arr += 2 #Numbers on main diagonal
+    u_arr = TDMASpecial(d_arr,b_arr,i)
+    exact_arr = analyticSolution(x)
+    #Plot
+    plot(x,u_arr, label = "n = " + str(i))
+    if i == n[-1]:
+        plot(x,exact_arr, label="Analytical solution")
 
 #LU-decomposition
 
 for i in n:
-    u = LUdecomp(i)
-    if i == n[-1]:
-        x = linspace(0,1,i)
-        plot(x,analyticSolution(x), label="Analytical solution")
+    u_arr = LUdecomp(i)
+    x = linspace(0,1,i)
+    plot(x,u_arr,label="LU-Decomp, n = "+ str(i))
 
 #Show plot
 '''

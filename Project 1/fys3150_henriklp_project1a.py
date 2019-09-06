a88ca6a70f66bb2022b129b182ec067effb3f9d5
henrikx2 <hlpetlund@gmail.com>
2019-09-06 13:06:26 +0200

    Main project working.

--- Project 1/fys3150_henriklp_project1a.py
+++ Project 1/fys3150_henriklp_project1a.py
@@ -2,7 +2,7 @@ from numpy import *
 from matplotlib.pyplot import *
 import sys
 import time
-from scipy.linalg import solve, lu
+from scipy.linalg import lu_solve, lu_factor
 
 #User interface Q&A
 print("This program solves a linear second order-differential equation by using a tridiagonal matrix derived from an approximation of the 2nd derivative. The calculation speeds and relative error will be determined in the case of a general algorithm, a special algorithm and by the use of LU-decomposition. The relative error will be shown as a function of mesh points.")
@@ -50,38 +50,33 @@ def TDMASpecial(d,b,v,n):
     return v
 
 def LUdecomp(n):
-    h = 1/(n+1) #Step size
+    h = 1/(n+2) #Step size
     A = zeros((n,n))
     b = zeros(n)
-    x = zeros(n)
     A[0,0] = 2; A[0,1] = -1; #Set first row of A
-    x = linspace(h,1-h,n); b[0] = h**2*f(x[0])
-    x[n-1] = n*h; b[n-1] = h**2*f(x[n-1])
+    x_lu = linspace(h,1-h,n); b[0] = h**2*f(x_lu[0])
+    x_lu[n-1] = n*h; b[n-1] = h**2*f(x_lu[n-1])
     for i in range(1,n-1): #Set middle of A
-        x[i] = x[i] + h
-        b[i] = h**2*f(x[i])
+        x_lu[i] = x_lu[i] + h
+        b[i] = h**2*f(x_lu[i])
         A[i,i-1] = -1 #Lower diagonal
         A[i,i] = 2 #Main diagonal
         A[i,i+1] = -1 #Upper diagonal
     A[n-1,n-1] = 2; A[n-1,n-2] = -1 #Set last row of A
-    L, U, piv = lu(A)
+    lu, piv = lu_factor(A)
     start_time = float(time.perf_counter())
-    z = solve(U,b)
-    v = solve(L,z)
+    v = lu_solve((lu,piv),b)
     elapsed_time = time.perf_counter() - start_time
     print("(n = "+str(n)+")[LU-Decomp], CPU Time: "+str(elapsed_time))
     return v
 
-relative_error = zeros(len(n)) #Initialize relative error arroy
-
-for i in n:
-    h = 1/(i+2) #Step size
-    x = linspace(0,1,i+2) #x-array
-    exact_arr = analyticSolution(x) #Analytical solution
-
-    #General Solution
-    v_g = zeros(i+2)
-    if g_ask == "y":
+#General algorithm
+if g_ask == "y":
+    for i in n:
+        h = 1/(i+2) #Step size
+        x_ = linspace(0,1,i+2) #x-array
+        exact_arr = analyticSolution(x) #Analytical solution
+        v_g = zeros(i+2)
         b_g = h**2*f(x) #Source term
         a_g = zeros(i)
         a_g += -1 #Numbers on lower diagonal
@@ -91,51 +86,62 @@ for i in n:
         c_g += -1 #Numbers on upper diagonal
         v_g = TDMAGeneral(a_g,d_g,c_g,b_g,v_g,i) #Activation general algorithm
 
-    #Special solution
-    v_s = zeros(i+2)
-    if s_ask == "y":
-        b_s = h**2*f(x) #Source term
-        d_s = zeros(i)
-        d_s[0] = 2 #First number in diagonal-array
-        for k in range(1,i): #Setting rest of diagonal-array before calculation
-            d_s[k] = (k+2)/(k+1)
-        v_s = TDMASpecial(d_s,b_s,v_s,i) #Activation special algorithm
+        plot(x,v_g, label = "n = " + str(i))
+        if i == n[-1]:
+            plot(x,exact_arr, label="Analytical solution")
 
+    title("General algorithm")
+    xlabel("x")
+    ylabel("v(x)")
+    legend()
 
-    #LU-decomposition
-    v_lu =zeros(i)
-    if lu_ask == "y" and i <= pow(10,4):
-        v_lu = append([0],append(LUdecomp(i),[0])) #Activation LU-Decomposition
-    if i > pow(10,4): #Trying LU-Decomposition with more than 10000x10000 matrix elements
-        try:
+#Special algorithm
+if s_ask == "y":
+    for i in n:
+        h = 1/(i+2) #Step size
+        x = linspace(0,1,i+2) #x-array
+        exact_arr = analyticSolution(x) #Analytical solution
+
+        v_s = zeros(i+2)
+        if s_ask == "y":
+            b_s = h**2*f(x) #Source term
+            d_s = zeros(i)
+            d_s[0] = 2 #First number in diagonal-array
+            for k in range(1,i): #Setting rest of diagonal-array before calculation
+                d_s[k] = (k+2)/(k+1)
+            v_s = TDMASpecial(d_s,b_s,v_s,i) #Activation special algorithm
+
+        plot(x,v_s, label = "n = " + str(i))
+        if i == n[-1]:
+            plot(x,exact_arr, label="Analytical solution")
+
+    title("Special algorithm")
+    xlabel("x")
+    ylabel("v(x)")
+    legend()
+
+#LU-Decomposition
+if lu_ask == "y":
+    for i in n:
+        x = linspace(0,1,i+2) #x-array
+        exact_arr = analyticSolution(x) #Analytical solution
+
+        v_lu = zeros(i)
+        if lu_ask == "y" and i <= pow(10,4):
             v_lu = append([0],append(LUdecomp(i),[0])) #Activation LU-Decomposition
+        if i > pow(10,4): #Trying LU-Decomposition with more than 10000x10000 matrix elements
+            try:
+                v_lu = append([0],append(LUdecomp(i),[0])) #Activation LU-Decomposition
+            except:
+                print("(n = "+str(i)+") Unable to allocate array with shape ("+str(i)+", "+str(i)+").")
+                break;
 
-        except:
-            print("Unable to allocate array with shape ("+str(i)+", "+str(i)+").")
+        plot(x,v_lu, label = "n = " + str(i))
+        if i == n[-1]:
+            plot(x,exact_arr, label="Analytical solution")
 
-
-    #Relative error
-    relative_error[int(log10(i))-1] = max((exact_arr[1:-1] - v_s[1:-1])/exact_arr[1:-1])
-
-#Plots
-    #SOLUTIONS
-    #plot(x,v_g, label = "n = " + str(i)))
-    #plot(x,v_s, label = "n = " + str(i))
-    #plot(x,v_lu,label="LU-Decomp, n = "+ str(i))
-    '''
-    if i == n[-1]:
-        plot(x,exact_arr, label="Analytical solution")
-    '''
-    #title("Relative error")
-    #xlabel("x")
-    #ylabel("Log($\epsilon$)")
-
-#Plots
-
-plot_error_ask = input("Plot relative error?\n(y/n): ")
-if plot_error_ask == "y":
-    plot(log10(n),relative_error)
-    title("Relative error of $Log_{10}$(n)")
-    xlabel("$Log_{10}$(n)")
-    ylabel("\epsilon(n)")
+    title("LU-Decomposition")
+    xlabel("x")
+    ylabel("v(x)")
+    legend()
     show()
